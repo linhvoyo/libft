7384f116c57d1a60d4109ba2a14ca7d4481f3d4f
Linh Lam <lilam@e1z2r3p3.42.us.org>
2017-12-05 18:02:45 -0800

    fix ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,26 +6,47 @@
 /*   By: lilam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 23:23:20 by lilam             #+#    #+#             */
-/*   Updated: 2017/12/05 17:58:07 by lilam            ###   ########.fr       */
+/*   Updated: 2017/12/05 18:02:43 by lilam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int    cal_length(char const *s, char c)
-{
-	int i;
-	int count;
+//static int    cal_length(char const *s, char c)
+//{
+//	int i;
+//	int count;
 
+//	i = 0;
+//	count = 0;
+//	while (s[i])
+//	{
+//		if (s[i] != c && s[i - 1] == c)
+//			count++;
+//		i++;
+//	}
+//	return (count)
+//}
+
+static int        cal_length(char const *str, char c)
+{
+	int    words;
+	int    i;
+
+	words = 0;
 	i = 0;
-	count = 0;
-	while (s[i])
+	while (str[i])
 	{
-		if (s[i] != c && s[i + 1] == c)
-			count++;
-		i++;
+		if (str[i] == c)
+			i++;
+		else
+		{
+			words++;
+			while (str[i] != '\0' && str[i] != c)
+				i++;
+		}
 	}
-	return (count);
+	return (words);
 }
 
 static char		*ft_print_word(int start, char const *str, char c)
